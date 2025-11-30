1e3acb03198b1fbbe3394d3467e1449fe6ff0f21
Shubh Agrawal <shubh7185@gmail.com>
2025-11-30 21:59:04 +0530

    Post

--- src/test/java/userManagement/postUsers.java
+++ src/test/java/userManagement/postUsers.java
@@ -249,7 +249,7 @@ public class postUsers {
         System.out.println(response.getBody().asString());
     }
 
-    @Test(description = "This method is used to Validate post with POJO")
+    @Test(description = "This  is used to Validate post with POJO")
     public void validatePostWithPOJODeserializationComplexArray() throws IOException
     {
         List<String> listLanguages = new ArrayList<>();
